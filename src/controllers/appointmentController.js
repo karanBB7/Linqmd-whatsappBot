35da26a97c87dd52d33e2788e058c0bc7caaa351
karanbojappa7 <karanbojappa7@gmail.com>
2024-09-03 23:57:16 +0530

    perfect and beautifull

--- src/controllers/appointmentController.js
+++ src/controllers/appointmentController.js
@@ -1,13 +1,11 @@
-const { createToken, getUserToken, setUserToken } = require('../middleware/tokenMiddleware');
-const { sendWhatsAppMessage, sendListMessage } = require('../middleware/whatsappMiddleware');
-const { checkAppointment } = require('../services/viewService');
-const { DateTime } = require('luxon');
+const { getUserToken} = require('../middleware/tokenMiddleware');
+const { sendWhatsAppMessage} = require('../middleware/whatsappMiddleware');
 const { setUserState, getUserState, clearUserState } = require('../services/stateManager');
 
 
-
+const { handleInitialMessage } = require('../handllers/mainHandler.js');
 const { handleCancelAppointment, handleDropStatus } = require('../handllers/cancelHandler');
-const { handleViewAppointment, otherAppointments } = require('../handllers/viewHandlers');
+const { handleViewAppointment } = require('../handllers/viewHandlers');
 const { handleFeedback, captureFeedback, captureReasonForVisit, captureOvercome, captureRating } = require('../handllers/feedbackHandler.js');
 
 const commandHandlers = {
@@ -53,72 +51,6 @@ async function handleIncomingMessage(message) {
     clearUserState(fromNumber);
   }
 }
-
-
-async function handleInitialMessage(fromNumber) {
-  try {
-    const appointmentData = await checkAppointment(fromNumber);
-    
-    if (appointmentData.success === 'true') {
-      const token = createToken(appointmentData.Username, fromNumber);
-      setUserToken(fromNumber, token);
-      // console.log("token", token);
-
-      const date = DateTime.fromISO(appointmentData.date);
-      const formattedDate = date.toFormat('cccc d LLLL');
-
-      if (appointmentData.appointment_tense === 'future') {
-        const message = `Dear ${appointmentData.patient_name}, You have an appointment with ${appointmentData.Docfullname} at ${appointmentData.clinic_name} on ${new Date(appointmentData.date).toLocaleDateString('en-US', { weekday: 'long', month: 'long', day: 'numeric', year: 'numeric' })} at ${appointmentData.slotTime}.`;
-        
-        await sendWhatsAppMessage(fromNumber, message);
-        await new Promise(resolve => setTimeout(resolve, 1000));
-        const listMessage = {
-          title: 'Do you want to?',
-          body: 'Please select the respective activity.',
-          options: ['View Appointment', 'Cancel Appointment']
-        };
-        await sendListMessage(fromNumber, listMessage);
-        
-        setUserState(fromNumber, 'awaitingSelection');
-
-      } else {
-        const message = `Dear ${appointmentData.patient_name}, You previously visited ${appointmentData.Docfullname} on ${new Date(appointmentData.date).toLocaleDateString('en-US', { weekday: 'long', month: 'long', day: 'numeric', year: 'numeric' })} at ${appointmentData.slotTime}.`;
-        await sendWhatsAppMessage(fromNumber, message);
-        await handleOtherAppointments(fromNumber);
-      }
-    } else {
-      await sendWhatsAppMessage(fromNumber, "Sorry, we couldn't find any appointments for you.");
-    }
-  } catch (error) {
-    console.error('Error in handleInitialMessage:', error);
-    await sendWhatsAppMessage(fromNumber, "An error occurred while processing your request. Please try again later.");
-  }
-}
-
-
-async function handleOtherAppointments(fromNumber) {
-  try {
-    const checkforotherappointments = await otherAppointments(fromNumber);
-
-    await new Promise(resolve => setTimeout(resolve, 1000));
-
-    const listMessage = checkforotherappointments ? {
-      title: 'Do you want to?',
-      body: 'Please select the respective activity.',
-      options: ['Give us your feedback', 'View Appointment', 'Cancel Appointment']
-    } : {
-      title: 'Do you want to?',
-      body: 'Please select the respective activity.',
-      options: ['Give us your feedback']
-    };
-
-    await sendListMessage(fromNumber, listMessage);
-    setUserState(fromNumber, 'awaitingSelection');
-  } catch (error) {
-    console.error("An error occurred:", error);
-  }
-}
-
 
 
 async function handleSelection(fromNumber, listid) {

611ba39947160fcf2fd88150abe2b5053d769d74
karanbojappa7 <karanbojappa7@gmail.com>
2024-09-03 22:55:36 +0530

    perfect version

---
+++ models/feedback.js
@@ -0,0 +1,30 @@
+module.exports = (sequelize, DataTypes) => {
+  const Feedback = sequelize.define("Feedback", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    fromNumber: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    doctor: DataTypes.TEXT,
+    jsonData: {
+      type: DataTypes.JSON,
+      allowNull: false,
+      defaultValue: {}
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW
+    }
+
+  },
+  {
+    tableName: 'feedback',
+    timestamps: true 
+  });
+
+  return Feedback;
+};
